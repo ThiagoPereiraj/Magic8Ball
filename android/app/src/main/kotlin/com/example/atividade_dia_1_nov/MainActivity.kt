94c59f79719412122a116ff467c1af73d1a98ea3
Thiago Pereira <thiagopereiaj@hotmail.com>
2023-11-01 22:51:44 -0300

    Projeto Pronto

---
+++ android/app/src/main/kotlin/com/example/atividade_dia_1_nov/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atividade_dia_1_nov
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  void shakeBall() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextButton( 
+              onPressed: () {
+                print('Fui clicado'); 
+                shakeBall();
+              },
+              child: Image.asset('assets/ball$ballNumber.png'), 
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
